cce96dd9832f2fe76971a9baa7878f40a32bd14b
Noarkhh <jakubpryc@student.agh.edu.pl>
2022-11-07 18:04:00 +0100

    OOP: added integration tests to lab4, finished the assignment

--- OOPLabs/oolab/src/main/java/agh/ics/oop/World.java
+++ OOPLabs/oolab/src/main/java/agh/ics/oop/World.java
@@ -5,12 +5,12 @@ import static java.lang.System.out;
 
 public class World {
     public static void main(String[] args) {
-        String[] args1 = new String[]{"f", "b", "r", "l", "f", "f", "r", "r", "f", "f"};
+        String[] args1 = new String[]{"f", "f", "f", "l", "f", "r", "r", "f", "l", "b", "b", "r"};
         MoveDirection[] directions = OptionsParser.parse(args1);
-        out.println(Arrays.toString(directions));
         IWorldMap map = new RectangularMap(10, 5);
-        Vector2d[] positions = { new Vector2d(2,2), new Vector2d(3,4) };
+        Vector2d[] positions = { new Vector2d(0, 0) };
         IEngine engine = new SimulationEngine(directions, map, positions);
+
         out.println(map);
         engine.run();
         out.println(map);
---
+++ OOPLabs/oolab/src/test/java/agh/ics/oop/SimulationTest.java
@@ -0,0 +1,38 @@
+package agh.ics.oop;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimulationTest {
+    @Test
+    public void testSimulation() {
+        String[] args1 = new String[]{"f", "b", "r", "l", "f", "f", "r", "r", "f", "f", "f", "f", "f", "f", "f", "f"};
+        MoveDirection[] directions = OptionsParser.parse(args1);
+        RectangularMap map = new RectangularMap(10, 5);
+        Vector2d[] positions = { new Vector2d(2,2), new Vector2d(3,4) };
+        IEngine engine = new SimulationEngine(directions, map, positions);
+        engine.run();
+
+        Assertions.assertTrue(map.isOccupied(new Vector2d(3, 4)));
+        Animal animal = (Animal) map.objectAt(new Vector2d(3, 4));
+        Assertions.assertEquals(MapDirection.NORTH, animal.getOrientation());
+
+        Assertions.assertTrue(map.isOccupied(new Vector2d(2, 0)));
+        animal = (Animal) map.objectAt(new Vector2d(2, 0));
+        Assertions.assertEquals(MapDirection.SOUTH, animal.getOrientation());
+    }
+
+    @Test
+    public void testSimulation2() {
+        String[] args1 = new String[]{"f", "f", "f", "l", "f", "r", "r", "f", "l", "b", "b", "r"};
+        MoveDirection[] directions = OptionsParser.parse(args1);
+        RectangularMap map = new RectangularMap(10, 10);
+        Vector2d[] positions = { new Vector2d(0, 0) };
+        IEngine engine = new SimulationEngine(directions, map, positions);
+        engine.run();
+
+        Assertions.assertTrue(map.isOccupied(new Vector2d(1, 1)));
+        Animal animal = (Animal) map.objectAt(new Vector2d(1, 1));
+        Assertions.assertEquals(MapDirection.EAST, animal.getOrientation());
+    }
+}

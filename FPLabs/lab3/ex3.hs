d6f004557715307a3d9af5a348217c20aed1151b
noarkhh <jakubpryc@student.agh.edu.pl>
2022-11-27 19:27:03 +0100

    FP: labs work

--- FPLabs/lab3/ex3.hs
+++ FPLabs/lab3/ex3.hs
@@ -8,3 +8,16 @@ funcFactory n = case n of
   _ -> const n
   where 
     intFunc x = x^5
+
+expApproxUpTo :: Int -> Double -> Double
+expApproxUpTo 0 x = 1 
+expApproxUpTo n x = ((x ^ n) / fromIntegral(fact n)) + expApproxUpTo (n - 1) x
+  where
+    fact :: Int -> Int
+    fact 0 = 1
+    fact n = n * fact (n - 1)
+
+dfr :: (Double -> Double) -> Double -> (Double -> Double)
+dfr f h = approx
+  where
+    approx x = (f (x + h) - f x) / h

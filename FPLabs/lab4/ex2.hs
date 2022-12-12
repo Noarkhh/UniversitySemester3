1269afa32d5ebe08a767056cc26f60fbf49485ba
noarkhh <jakubpryc@student.agh.edu.pl>
2022-12-12 15:50:19 +0100

    FP: labs

---
+++ FPLabs/lab4/ex2.hs
@@ -0,0 +1,28 @@
+data CartInt2DVec = MkCartInt2DVec Int Int
+
+xCoord :: CartInt2DVec -> Int
+xCoord (MkCartInt2DVec x _) = x
+
+yCoord :: CartInt2DVec -> Int
+yCoord (MkCartInt2DVec _ y) = y
+
+
+data Cart2DVec' a = MkCart2DVec' {x::a, y::a}
+
+
+data List a = EmptyL | Cons a (List a) deriving Show
+
+head' :: List a -> a
+head' EmptyL      = error "head': the empty list has no head!"
+head' (Cons x xs) = x
+
+data ThreeColors = Blue |
+                   White |
+                   Red
+
+type ActorName = String
+
+leadingActor :: ThreeColors -> ActorName
+leadingActor Blue  = "Juliette Binoche"
+leadingActor White = "Zbigniew Zamachowski"
+leadingActor Red   = "Irene Jacob"
